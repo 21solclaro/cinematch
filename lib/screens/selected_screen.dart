fb7fa378a72c42a2e962a41ce2cfdc709395f109
yoh <yoh@21solclaro.me>
2024-02-27 13:00:13 +0900

    add: Implement screen to display selected search criteria.

---
+++ lib/screens/selected_screen.dart
@@ -0,0 +1,53 @@
+import 'package:cinematch/models/selection_criteria.dart';
+import 'package:cinematch/providers/selection_provider.dart';
+import 'package:cinematch/screens/movie_list_screen.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+class SelectedScreen extends ConsumerWidget {
+  const SelectedScreen({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final SelectionCriteria selection = ref.watch(selectionCriteriaProvider);
+
+    return Scaffold(
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Text('Providers: ${selection.providers.join(',')}'),
+              const SizedBox(height: 20),
+              Text('Genres: ${selection.genres.join(',')}',
+                  textAlign: TextAlign.center),
+              const SizedBox(height: 20),
+              Text('Runtime: ${selection.runtime.toString()} min',
+                  textAlign: TextAlign.center),
+            ],
+          ),
+        ),
+      ),
+      bottomNavigationBar: Padding(
+        padding: const EdgeInsets.all(32.0),
+        child: ElevatedButton(
+          onPressed: () {
+            Navigator.push(
+                context,
+                MaterialPageRoute(
+                    builder: (context) => const MovieListScreen()));
+          },
+          style: ElevatedButton.styleFrom(
+            minimumSize: const Size(double.infinity, 50),
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(10),
+            ),
+          ),
+          child: const Text('Match!'),
+        ),
+      ),
+    );
+  }
+}
